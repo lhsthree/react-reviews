f2728a11bebb52349d347f1b67b4326a70503817
lhsthree <lhsthree@gmail.com>
2021-02-01 14:33:36 -0600

    feat: add random surprise me button

--- src/Review.js
+++ src/Review.js
@@ -29,6 +29,13 @@ const Review = () => {
 		return checkNumber(newIndex)
 		})
 	}
+	const randomPerson = () =>{
+		let randomNumber = Math.floor(Math.random()*people.length)
+		if(randomNumber === index){
+			randomNumber = index +1
+		}
+		setIndex(checkNumber(randomNumber))
+	}
 
   return (
   	<article className="review">
@@ -49,7 +56,7 @@ const Review = () => {
     			<FaChevronRight />
     		</button>
     	</div>	
-    		<button className="random-btn">
+    		<button className="random-btn" onClick={randomPerson}>
     			Surprise Me
     		</button>	
     </article>
